eaed6ae7eada4b12d90044669c7a48437280aa19
carlubian <carlubian@outlook.es>
2016-02-21 10:56:43 +0100

    Ahora devuelve un error 404 si no encuentra al usuario

--- src/main/java/hello/MainController.java
+++ src/main/java/hello/MainController.java
@@ -10,7 +10,6 @@ import javax.persistence.Persistence;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,7 +23,7 @@ public class MainController {
         
         if (result != null)
         	return result;
-        return null;
+        throw new UsuarioNoEncontrado();
     }
 
     private UserInfo buscarUsuario(String email, String password) {
